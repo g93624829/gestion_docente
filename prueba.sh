a2cd1000823702512a235a5d42d0d06199ea2549
g93624829 <g93624829@gmail.com>
2024-12-14 18:10:09 -0500

    Noveno commit
    Noveno commit

---
+++ prueba.sh
@@ -0,0 +1,19 @@
+echo "¡Hola, Mundo!"
+
+# Crear una carpeta llamada 'ejemplo' en el directorio actual
+mkdir ejemplo
+
+# Cambiar al directorio 'ejemplo'
+cd ejemplo
+
+# Crear un archivo de texto llamado 'archivo.txt' y agregar algo de contenido
+echo "Este es un archivo de ejemplo" > archivo.txt
+
+# Mostrar el contenido del archivo creado
+cat archivo.txt
+
+# Volver al directorio anterior
+cd ..
+
+# Mostrar un mensaje final
+echo "El script ha terminado de ejecutarse."
